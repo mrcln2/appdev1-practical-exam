0f1b8b86d57468808865d6fe2900e4279ddbb214
mrcln2 <marclean0965@gmail.com>
2026-08-01 13:28:08 +0800

    Practical Exam Part 4: JSX expressions implemented

--- src/components/tasks/TaskItem.jsx
+++ src/components/tasks/TaskItem.jsx
@@ -1,17 +1,34 @@
 import { TASK_CATEGORY, TASK_STATUS } from './taskConstants';
 
+function getTaskMessage() {
+  return 'Keep going!';
+}
+
 function TaskItem({ task, description }) {
+  const userName = 'Marclean';
+  const taskType = 'Task';
+
+  const isCompleted = false;
+
   return (
     <>
       <article>
         <h3>{task}</h3>
+
         <p>{description}</p>
+
+        <p>{`${userName}'s ${taskType}`}</p>
+
+        <p>Task Number: {1 + 1}</p>
+
+        <p>{isCompleted ? 'Completed' : 'Not Completed'}</p>
+
         <small>Category: {TASK_CATEGORY}</small>
         <br />
         <small>Status: {TASK_STATUS}</small>
       </article>
 
-      <p>Ready to get this done?</p>
+      <p>{getTaskMessage()}</p>
     </>
   );
 }
